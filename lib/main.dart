948f6c0f1def399b2ce2be5b5eba2c1d07357f0f
AdrianICH <adriancuerva2004@gmail.com>
2023-08-18 12:51:30 -0500

    cambios a las direcciones de los import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:principal_shalom/controllers/controlador_general.dart';
-import 'ui/login_page.dart';
+import 'ui/login/login_page.dart';
 
 void main() {
-  Get.put(ControlPersonas());
+  Get.put(ControlUsuarios());
   runApp(const Login());
 }
