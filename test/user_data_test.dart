9b171fbce776811958dc9856bba7e56ab38b3fbd
AdrianICH <adriancuerva2004@gmail.com>
2023-08-25 14:11:00 -0500

    Unit Testing added

---
+++ test/user_data_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:principal_shalom/api/consulta_tipos.dart';
+import 'package:principal_shalom/api/consulta_usuario.dart';
+import 'package:principal_shalom/api/eliminar_usuario.dart';
+import 'package:principal_shalom/api/insertar_persona.dart';
+import 'package:principal_shalom/controllers/controlador_general.dart';
+
+void main() {
+  test('Testeo de llamada usuario desde API y respuesta', () async {
+    final respuesta = await consultaUsuario("Ambaru", "9091");
+
+    expect(respuesta, isInstanceOf<List<UserLoggedInfo>>(),
+        reason:
+            "Verificar si la instancia devuelta es de tipo List<UserLoggedInfo>");
+    expect(respuesta.isEmpty, false,
+        reason: "Verificar si la respuesta contiene datos de usuario");
+  });
+
+  test('Testeo de llamada nombre de usuario desde API y controlador', () async {
+    Get.put(ControlUsuarios());
+    ControlUsuarios control = Get.find();
+    final respuesta = await consultaUsuario("Ambaru", "9091");
+
+    control.cargarUsuario(respuesta);
+    expect(control.consulta?[0].NOMBRE, "Ambar Isbel",
+        reason:
+            "Verificar si los datos llamados del usuario son correctos/no están manipulados");
+  });
+
+  test('Testeo de llamada a los tipos de usuario desde API y controlador',
+      () async {
+    Get.put(ControlUsuarios());
+    ControlUsuarios control = Get.find();
+    final respuesta = await consultaTipos();
+    control.cargarTipos(respuesta);
+    expect(control.types, isInstanceOf<List<UserTypes>>(),
+        reason:
+            "Verificar si los datos llamados de los tipos de usuario son correctos/no están manipulados");
+
+    var verificacionResult = await control.verificacion("Estudiante");
+    expect(verificacionResult, "1",
+        reason:
+            "Verificar si los datos son correctos al llamar desde el formulario (Estudiante)");
+
+    verificacionResult = await control.verificacion("Profesor");
+    expect(verificacionResult, "2",
+        reason:
+            "Verificar si los datos son correctos al llamar desde el formulario (Profesor)");
+  });
+
+  test('Testeo de agregación de usuarios', () async {
+    Get.put(ControlUsuarios());
+    ControlUsuarios control = Get.find();
+    var respuesta = await insertarPersona(
+        "adriactor63", "qscxzsewaxd", "Adrian Hernandez", "1114902470", "2");
+    control.guardarUsuario(respuesta);
+    expect(control.datos![0].USUARIO, "adriactor63");
+  });
+
+  test('Testeo de eliminacion de usuarios', () async {
+    var sndRespuesta = await eliminarUser("adriactor63");
+    if (sndRespuesta != "El usuario no existe o ya ha sido eliminado") {
+      expect(sndRespuesta, "Se ha eliminado el usuario: adriactor63");
+    }
+  });
+}
